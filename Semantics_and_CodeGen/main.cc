fcc46969e50a6981d7f98cdd04b1af33c8e8feff
Umesh Kalvakuntla <umeshkalvakuntla26@gmail.com>
2022-11-21 18:32:14 +0530

    Modified Makefile

--- Semantics_and_CodeGen/main.cc
+++ Semantics_and_CodeGen/main.cc
@@ -17,11 +17,11 @@ int main()
   yy::parser parser(ctx);
   parser.parse();
 
-  if (ctx.error_list.size())
-  {
-    for (auto &v : ctx.error_list)
-      ctx.error(v.first, v.second);
-  }
+  // if (ctx.error_list.size())
+  // {
+  //   for (auto &v : ctx.error_list)
+  //     ctx.error(v.first, v.second);
+  // }
   // else
   // {
   //   ctx.dump_ast();
