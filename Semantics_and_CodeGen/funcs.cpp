c2774d3a8d8b22b012a40b4c957021d882a8b958
Vasishtadepu <agma6666@gmail.com>
2022-11-25 17:33:45 +0530

    Added DFS and has cycle to funcs.cpp

--- Semantics_and_CodeGen/funcs.cpp
+++ Semantics_and_CodeGen/funcs.cpp
@@ -52,30 +52,83 @@ extern "C"
     printf("\n");
   }
 
-  // int visited_dfs[1000];
-  // int dfs_final[1000];
-  // int pos_dfs = 0;
+void main_dfs(int * matrix, int size, int root, int * dfs_final)
+{
+    int visited_dfs[size];
+    for(int i = 0;i<size;i++)
+    {
+        visited_dfs[i] = 0;
+    }
+    int pos_dfs = 0;
+    int st[1000];
+    int top = 0;
+    st[top] = root;
+    top++;
+    while(top)
+    {
+        int t = st[top-1];
+        top--;
+        visited_dfs[t] = 1;
+        dfs_final[pos_dfs] = t;
+        pos_dfs++;
+        for(int i = 0 ; i<size; i++)
+        {
+            if (*((matrix + t * size) + i) == 1 && visited_dfs[i] == 0)
+            {
+                st[top] = i;
+                top++; 
+            }
+        }
+    }
+    for(int i = 0;i<size;i++)
+    {
+      printf("%d ",dfs_final[i]);
+    } 
+    printf("\n");
+}
 
-  // void sub_DFS(int *matrix, int size, int root)
-  // {
-  //   dfs_final[pos_dfs] = root;
-  //   pos_dfs++;
-  //   visited_dfs[root] = 1;
-  //   for (int j = 0; j < size; j++)
-  //   {
-  //     if (*((matrix + root * size) + j) == 1 && !visited_dfs[j])
-  //     {
-  //       sub_DFS(matrix, size, j);
-  //     }
-  //   }
-  // }
 
-  int *main_dfs(int *matrix, int size, int root)
-  {
-    // sub_DFS(matrix, size, root);
-    printf("DFS func in funcs.cpp\n");
-    return nullptr;
-  }
+int has_cycle(int * matrix, int size)
+{
+    int bfs_final[1000];
+    int parent_arr[size];
+    for(int i = 0;i<size;i++)
+    {
+      parent_arr[i] = -1;
+    }
+    int visited_bfs[1000];
+    int qu[1000];
+    int front = 0;
+    int back = 0;
+
+    visited_bfs[root] = 1;
+    qu[back] = root;
+    back++;
+    while (front != back)
+    {
+      int t = qu[front];
+      front++;
+      for (int i = 0; i < size; i++)
+      {
+        if (*((matrix + t * size) + i) == 1)
+        {
+          if(!visited_bfs[i])
+          {
+            visited_bfs[i] = 1;
+            qu[back] = i;
+            parent_arr[i] = t;
+          }
+          else if(parent_arr[i]!=t)
+          {
+            return 1;
+          }
+        }
+      }
+    }
+    return 0;
+}
+
+
   void dijkstras(int *matrix, int size, int root, int *dists) {
     bool sptSet[size];
     for (int i = 0; i < size; i++){
